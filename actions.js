b3a5668c568396e1b31a1095a65c2ed7e51b5131
Karolina Siwkowska <trallala00@gmail.com>
2018-05-02 16:58:43 +0200

    Add actions and types of actions

--- actions.js
+++ actions.js
@@ -1,1 +1,35 @@
 import uuid from uuid;
+
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const INCREMENT_COMMENT = 'INCREMENT_COMMENT';
+export const DECREMENT_COMMENT = 'DECREMENT_COMMENT';
+
+
+const addComment = text => {
+    type: ADD_COMMENT,
+    text,
+    id: uuid.v4()
+}
+
+const removeComment = id => {
+    type: REMOVE_COMMENT,
+    id
+}
+
+const editComment = (id, text) => {
+    type: EDIT_COMMENT,
+    id,
+    text
+}
+
+const incrementComment = id => {
+    type: INCREMENT_COMMENT,
+    id
+}
+
+const decrementComment = id => {
+    type: DECREMENT_COMMENT,
+    id
+}
